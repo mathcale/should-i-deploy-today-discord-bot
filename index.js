a01c9d553cd37ef9c4b6d380ae15ac5e3ff4eb01
Matheus Calegaro <matheuscalegaro@outlook.com>
2020-07-17 16:02:06 +0000

    🍲🚍 Checkpoint ./index.js:1277432/1031 ./package.json:1277432/1971

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const Discord = require('discord.js');
+const fetch = require('isomorphic-unfetch');
 
 const app = express();
 const client = new Discord.Client();
@@ -10,15 +11,31 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'pong!' });
 });
 
+app.listen(process.env.PORT, () => {
+  console.log(`${new Date().toISOString()} INFO - App started on port ${process.env.PORT}!`);
+});
+
+setInterval(() => {
+  http.get(`${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', () => {
-  
+const prefix = '!';
+
+client.on('message', async message => {
+  if (message.content.startsWith(`${prefix}deploy`)) {
+    try {
+      const response = await fetch('https://shouldideploy.today/api?tz=America/Sao_Paulo');
+      const data = await response.json();
+
+      message.channel.sendMessage(`${data.shouldideploy ? '👍' : '👎'} ${data.message}`);
+    } catch (err) {
+      message.channel.sendMessage(`❌ ERROR! ${err.message}`);
+    }
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
-app.listen(process.env.PORT, () => {
-  console.log(${new Date().toISOString()} INFO - App started!`)
-});
