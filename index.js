bddfba2f5427eadf2b5382b0e8b7a4e37c8612db
Matheus Calegaro <matheuscalegaro@outlook.com>
2020-07-17 18:01:06 +0000

    💕⛺️ Checkpoint ./index.js:1277432/2401

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const http = require('http');
-const express = require('express');
-const Discord = require('discord.js');
-const fetch = require('isomorphic-unfetch');
+const http = require("http");
+const express = require("express");
+const Discord = require("discord.js");
+const fetch = require("isomorphic-unfetch");
 
 const app = express();
 const client = new Discord.Client();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log(`${new Date().toISOString()} INFO - Ping received`);
-  res.status(200).json({ message: 'pong!' });
+  res.status(200).json({ message: "pong!!" });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`${new Date().toISOString()} INFO - App started on port ${process.env.PORT}!`);
+  console.log(
+    `${new Date().toISOString()} INFO - App started on port ${
+      process.env.PORT
+    }!`
+  );
 });
 
 setInterval(() => {
   http.get(`https://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
-client.once('ready', () => {
-	console.log(`${new Date().toISOString()} INFO - Bot ready!`);
+client.once("ready", () => {
+  console.log(`${new Date().toISOString()} INFO - Bot ready!!`);
 });
 
-const prefix = '!';
+const prefix = "!";
 
-client.on('message', async message => {
+client.on("message", async message => {
   if (message.content.startsWith(`${prefix}candeploy`)) {
     try {
-      const response = await fetch('https://shouldideploy.today/api?tz=America/Sao_Paulo');
+      const response = await fetch(
+        "https://shouldideploy.today/api?tz=America/Sao_Paulo"
+      );
       const data = await response.json();
 
-      message.channel.send(`${data.shouldideploy ? '👍' : '👎'} ${data.message}`);
+      message.channel.send(
+        `${data.shouldideploy ? "👍" : "👎"} ${data.message}`
+      );
     } catch (err) {
       message.channel.send(`❌ ERROR! ${err.message}`);
     }
